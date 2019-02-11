3ed9f3face6197135d2a5aa3df94074f28c2ea81
CloudSu <BetaPhoenixSNSD@gmail.com>
2019-02-12 03:01:18 +0800

    修正路徑

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/PlayStation-admin-3.0/dist/',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
