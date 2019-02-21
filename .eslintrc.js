b329802d536361294e064b7bbb912a5a55b3627f
CloudSu <BetaPhoenixSNSD@gmail.com>
2019-02-22 02:09:11 +0800

    修正 eslint 設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,12 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-unused-vars": 0,
+    "no-console": 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-param-reassign": [2, { props: false }],
+    'linebreak-style': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
